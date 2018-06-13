ade043f9c1df5c9d3e7de6693547122c540ee414
kammitama5 <krystal.maughan@gmail.com>
2018-06-13 10:34:38 -0700

    Add files via upload

---
+++ HundredEleven.hs
@@ -0,0 +1,18 @@
+module HundredEleven where
+
+-- does it compile (they all do now)
+
+a = (+)
+b = 5
+c = a b 10
+d = a c 200
+
+a' = 12 + b
+b' = 10000 * c
+
+x = print "hello world"
+y = print "woohoo!"
+z = "hello world"
+
+bigNum = ((^) 5) 10
+wahoo = bigNum * 10
