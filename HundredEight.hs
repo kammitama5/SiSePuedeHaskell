d7120bfd4aba7109e03a2fffa1fd49c4e9a24de7
kammitama5 <krystal.maughan@gmail.com>
2018-06-13 09:19:18 -0700

    Add files via upload

---
+++ HundredEight.hs
@@ -0,0 +1,25 @@
+module HundredEight where
+
+--(++) :: [a] -> [a] -> [a]
+-- type: [a] -> [a] -> [a]
+
+-- myConcat x = x ++ " yo"
+-- [Char] -> [Char]
+
+--(*) :: Num a => a -> a -> a
+-- type : Num a => a -> a -> a
+
+-- myMult x = (x / 3) * 5
+-- type : Fractional a => a -> a
+
+-- take :: Int -> [a] -> [a]
+-- myTake x = take x "hey you"
+-- type : Int -> [Char]
+
+-- (>) :: Ord a => a -> a -> Bool
+-- myCom x = x > (length [1..10])
+-- type : Int -> Bool
+
+-- (<) :: Ord a => a -> a -> Bool
+-- myAlph x = x < 'z'
+-- type : Char -> Bool
