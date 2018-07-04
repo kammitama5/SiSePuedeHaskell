6fec07412b23f371bc8e8fcdcb16e0116249e2cd
kammitama5 <krystal.maughan@gmail.com>
2018-07-04 12:55:37 -0700

    added NoRestrictMonomorphism lang ext

---
+++ Twelvea.hs
@@ -0,0 +1,34 @@
+{-# LANGUAGE NoMonomorphismRestriction #-}
+
+module Twelvea where
+
+    --x = 5
+    --y = x + 5
+    --w = y * 10
+
+    -- w = Num a => a
+
+    --x = 5
+    --y = x + 5
+    --z y = y * 10
+
+    -- z = Num a => a -> a
+
+    --x = 5
+    --y = x + 5
+    --f = 4 / y
+
+    -- f = Fractional a => a
+
+    --x = "Julie"
+    --y = " <3 "
+    --z = "Haskell"
+    --f = x ++ y ++ z
+
+    -- f :: [Char]
+
+ 
+
+
+
+
